33ce6a85e1720339d1606b5f70e36c458b985c2c
udittyagi455 <udittyagi455@gmail.com>
2024-03-20 23:38:29 +0530

    work on topics and authors api

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import Jwt from "jsonwebtoken";
+import fs from "fs"
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -27,8 +28,6 @@ const registerUser = asyncHandler(async (req, res) => {
     password,
     phoneNumber,
     country,
-    topics,
-    author,
     avatar
   );
 
@@ -94,4 +93,124 @@ const uploadAvatar = asyncHandler(async (req, res) => {
   });
 });
 
-export { registerUser, uploadAvatar };
+const topics = asyncHandler(async (req,res) => {
+  const topics = [
+    {
+      id: 1,
+      topic: "National"
+    },
+    {
+      id: 2,
+      topic: "International"
+    },
+    {
+      id: 3,
+      topic: "Sport"
+    },
+    {
+      id: 4,
+      topic: "Lifestyle"
+    },
+    {
+      id: 5,
+      topic: "Business"
+    },
+    {
+      id: 6,
+      topic: "Health"
+    },
+    {
+      id: 7,
+      topic: "Fashion"
+    },
+    {
+      id: 8,
+      topic: "Technology"
+    },
+    {
+      id: 9,
+      topic: "Science"
+    },
+    {
+      id: 10,
+      topic: "Art"
+    },
+    {
+      id: 11,
+      topic: "Poltics"
+    }
+  ]
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,topics,"All topics")
+  )
+})
+
+const authors = asyncHandler(async(req,res) => {
+  const author = [
+    {
+      id: 1,
+      image:  "http://res.cloudinary.com/dydqwz5bv/image/upload/v1710957881/etsbzbvdm4omrzactj6w.png",
+      name: 'BBC News',
+      follow: false,
+    },
+    {
+      id: 2,
+      image: "http://res.cloudinary.com/dydqwz5bv/image/upload/v1710957163/lataf5hzndqfjizvjsaa.png",
+      name: 'SCMP',
+      follow: false,
+    },
+      {
+        id: 3,
+        image: "http://res.cloudinary.com/dydqwz5bv/image/upload/v1710957163/lataf5hzndqfjizvjsaa.png",
+        name: 'CNN',
+        follow: false,
+      },
+      {
+        id: 4,
+        image: "http://res.cloudinary.com/dydqwz5bv/image/upload/v1710957163/lataf5hzndqfjizvjsaa.png",
+        name: 'MSN',
+        follow: false,
+      },
+      {
+        id: 5,
+        image: "http://res.cloudinary.com/dydqwz5bv/image/upload/v1710957163/lataf5hzndqfjizvjsaa.png",
+        name: 'CNET',
+        follow: false,
+      },
+      {
+        id: 6,
+        image: "http://res.cloudinary.com/dydqwz5bv/image/upload/v1710957163/lataf5hzndqfjizvjsaa.png",
+        name: 'USA Today',
+        follow: false,
+      },
+      {
+        id: 7,
+        image: "http://res.cloudinary.com/dydqwz5bv/image/upload/v1710957163/lataf5hzndqfjizvjsaa.png",
+        name: 'TIME',
+        follow: false,
+      },
+      {
+        id: 8,
+        image: "http://res.cloudinary.com/dydqwz5bv/image/upload/v1710957163/lataf5hzndqfjizvjsaa.png",
+        name: 'Buzzfeed',
+        follow: false,
+      },
+      {
+        id: 9,
+        image: "http://res.cloudinary.com/dydqwz5bv/image/upload/v1710957163/lataf5hzndqfjizvjsaa.png",
+        name: 'Daily Mail',
+        follow: false,
+      },
+  ];
+
+  return  res
+  .status(200)
+  .json(
+    new ApiResponse(200,author,"all authors")
+  )
+})
+
+export { registerUser, uploadAvatar,topics,authors }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { registerUser,uploadAvatar } from "../controllers/user.controllers.js";
+import { registerUser,topics,uploadAvatar,authors } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router();
 
 router.route("/register").post(registerUser)
 router.route("/upload-image").post(upload.single("file"),uploadAvatar)
+router.route("/topics").get(topics)
+router.route("/authors").get(authors)
 
 export default router;
